e0a865b9ea7fa3d969baaef152a0e0bc43cac9bf
Prachijakati <prachijakati@gmail.com>
2025-05-13 00:16:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model and scaler
+model = joblib.load("expense_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input data from the request
+    data = request.get_json()
+    
+    month = data['month']
+    year = data['year']
+    income = data['income']
+    festival_count = data['festival_count']
+
+    # Prepare input data for prediction
+    input_data = np.array([[month, year, income, festival_count]])
+    input_scaled = scaler.transform(input_data)
+    
+    # Predict the expenses
+    prediction = model.predict(input_scaled)[0]
+
+    # Return predictions as a JSON response
+    return jsonify({
+        "predictions": dict(zip([
+            'Food', 'Groceries', 'Transport', 'Entertainment', 
+            'Shopping', 'Rent', 'Bills', 'Healthcare', 'Education'
+        ], [round(val, 2) for val in prediction]))
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
