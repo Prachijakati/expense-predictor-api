a7da12498f683814fc8b479ee777dd91879220c6
Prachijakati <prachijakati@gmail.com>
2025-05-14 22:55:41 +0530

    Fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -33,5 +33,8 @@ def predict():
         ], [round(val, 2) for val in prediction]))
     })
 
+import os 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
